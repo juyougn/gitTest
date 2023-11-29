9d300e961d5a4e14fae3009be792810eda2b5019
jyp <sndi9207@naver.com>
2023-11-29 18:21:43 +0900

    헤더 작업중

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,82 +1,96 @@
-import { Link, useLocation  } from "react-router-dom";
-import './style/HeaderStyle.scss'
+import { Link, useLocation } from "react-router-dom";
+import './style/HeaderStyle.scss';
 import { useEffect, useState } from "react";
 
-
 function Header() {
-  const [activeTab, setActiveTab] = useState('홈');
+  const [activeTab, setActiveTab] = useState('');
   const location = useLocation();
 
   const handleClick = (tab) => {
     setActiveTab(tab);
   };
 
-  // 페이지가 처음 로딩될 때 activeTab 상태를 '홈'으로 초기화
-  useEffect(() => {
-    // Link로 이동할 때 location.pathname을 기반으로 activeTab을 설정
-    if (location.pathname === '/') {
-      setActiveTab('홈');
-    } else if (location.pathname === '/list') {
-      setActiveTab('병종');
-    } else if (location.pathname === '/map') {
-      setActiveTab('지도');
-    } else if (location.pathname === '/search') {
-      setActiveTab('가문');
-    }
-  }, [location]);
-  // activeTab 상태에 따라 동적으로 클래스를 적용합니다. 
-  // 만약 현재 탭이 '홈'이라면 active 클래스를 추가하고, 그렇지 않으면 추가하지 않습니다.
-  // 이를 통해 선택된 탭에 대한 스타일이 변경됩니다.
+ 
 
   return (
-    <div className="HeaderContainer"  >
-     
-      <ul className="Ul" style={{display:"flex", justifyContent:""}} >
+    <div className="HeaderContainer">
+      <ul className="Ul menu-item" style={{ display: "flex", justifyContent: "" }}>
+        <a href="/" aria-label="하나금융그룹" title="홈으로" className="bth-home">
+          <h1 className="blind" style={{ width: "100%" }}>하나금융그룹</h1>
+        </a>
+        <div>
+          <br />
+        </div>
+        <li className={`Li ${activeTab === '그룹소개' ? 'active' : ''}`}>
+          <Link to="/test4" className="AStyle" onClick={() => handleClick('그룹소개')}>
+            그룹소개
+             {/* 서브메뉴 시작 */}
+             <ul className="submenu">
+              <li><Link to="/submenu5">서브메뉴</Link></li>
+              <li><Link to="/submenu6">서브메뉴</Link></li>
+              <li><Link to="/submenu7">서브메뉴</Link></li>
+              <li><Link to="/submenu8">서브메뉴</Link></li>
         
-           
-              <img className="IMG" src="./img/logo/1img.webp" alt="" />
-            
-         
+            </ul>
+          </Link>
+        </li>
+        <li className={`Li ${activeTab === '기업지배구조' ? 'active' : ''}`}>
+          <Link to="/test4" className="AStyle" onClick={() => handleClick('기업지배구조')}>
+            기업지배구조
+             {/* 서브메뉴 시작 */}
+             <ul className="submenu">
+              <li><Link to="/submenu5">서브메뉴</Link></li>
+              <li><Link to="/submenu6">서브메뉴</Link></li>
+              <li><Link to="/submenu7">서브메뉴</Link></li>
+              <li><Link to="/submenu8">서브메뉴</Link></li>
         
-
+            </ul>
+          </Link>
+        </li>
+        <li className={`Li ${activeTab === 'ESG경영' ? 'active' : ''}`}>
+          <Link to="/test1" className="AStyle" onClick={() => handleClick('ESG경영')}>
+            ESG경영
+             {/* 서브메뉴 시작 */}
+             <ul className="submenu">
+              <li><Link to="/submenu5">서브메뉴</Link></li>
+              <li><Link to="/submenu6">서브메뉴</Link></li>
+              <li><Link to="/submenu7">서브메뉴</Link></li>
+              <li><Link to="/submenu8">서브메뉴</Link></li>
         
-          <li className={`Li ${activeTab === '홈' ? 'active' : ''}`}>
-            <Link to="/" className="AStyle" onClick={() => handleClick('홈')}>
-              홈
-            </Link>
-          </li>
-          <li className={`Li ${activeTab === '그룹소개' ? 'active' : ''}`}>
-            <Link to="/list" className="AStyle" onClick={() => handleClick('그룹소개')}>
-              그룹소개
-            </Link>
-          </li>
-          <li className={`Li ${activeTab === '기업지배구조' ? 'active' : ''}`}>
-            <Link to="/map" className="AStyle" onClick={() => handleClick('기업지배구조')}>
-              기업지배구조
-            </Link>
-          </li>
-          
-          <li className={`Li ${activeTab === 'ESG경영' ? 'active' : ''}`}>
-            <Link to="/test1" className="AStyle" onClick={() => handleClick('ESG경영')}>
-              ESG경영
-            </Link>
-          </li>
-          <li className={`Li ${activeTab === '투자정보' ? 'active' : ''}`}>
-            <Link to="/test2" className="AStyle" onClick={() => handleClick('투자정보')}>
+            </ul>
+          </Link>
+        </li>
+        <li className={`Li ${activeTab === '투자정보' ? 'active' : ''}`}>
+          <Link to="/test2" className="AStyle" onClick={() => handleClick('투자정보')}>
             투자정보
-            </Link>
-          </li>
-          <li className={`Li ${activeTab === 'PR센터' ? 'active' : ''}`}>
-            <Link to="/test3" className="AStyle" onClick={() => handleClick('PR센터')}>
+             {/* 서브메뉴 시작 */}
+             <ul className="submenu">
+              <li><Link to="/submenu5">서브메뉴</Link></li>
+              <li><Link to="/submenu6">서브메뉴</Link></li>
+              <li><Link to="/submenu7">서브메뉴</Link></li>
+              <li><Link to="/submenu8">서브메뉴</Link></li>
+        
+            </ul>
+          </Link>
+        </li>
+        <li className={`Li ${activeTab === 'PR센터' ? 'active' : ''}`}>
+          <Link to="/test3" className="AStyle" onClick={() => handleClick('PR센터')}>
             PR센터
-            </Link>
-          </li>
-      
-
-
+             {/* 서브메뉴 시작 */}
+             <ul className="submenu">
+              <li><Link to="/submenu5">서브메뉴</Link></li>
+              <br />
+              <li><Link to="/submenu6">서브메뉴</Link></li>
+              <li><Link to="/submenu7">서브메뉴</Link></li>
+              <li><Link to="/submenu8">서브메뉴</Link></li>
+        
+            </ul>
+          </Link>
+        </li>
+        
       </ul>
     </div>
-  )
+  );
 }
 
 export default Header;
